b60f7163bc8baf0f33bc851333316d8b8d3ecc69
Matt Pedersen <mpedersen2054@gmail.com>
2017-09-07 22:32:02 -0400

    add all The Atlantic URLs and make sure it works

--- scrapers/atlanticnews.js
+++ scrapers/atlanticnews.js
@@ -7,33 +7,46 @@ module.exports = class AtlanticNews extends Scraper {
         super()
         this.urls = [
             { title: 'politics_news', url: 'https://www.theatlantic.com/feed/channel/politics/', category: 'politics' },
-            // { title: 'entertainment_news', url: 'https://www.theatlantic.com/feed/channel/entertainment/', category: 'entertainment' },
-            // { title: 'technology_news', url: 'https://www.theatlantic.com/feed/channel/technology/', category: 'technology' },
-            // { title: 'health_news', url: 'https://www.theatlantic.com/feed/channel/health/', category: 'health' },
-            // { title: 'sexs_news', url: 'https://www.theatlantic.com/feed/channel/sexes/', category: 'misc' },
-            // { title: 'science_news', url: 'https://www.theatlantic.com/feed/channel/science/', category: 'science' },
-            // { title: '', url: '', category: '' },
-            // { title: '', url: '', category: '' },
-            // { title: '', url: '', category: '' },
-            // { title: '', url: '', category: '' },
+            { title: 'entertainment_news', url: 'https://www.theatlantic.com/feed/channel/entertainment/', category: 'entertainment' },
+            { title: 'technology_news', url: 'https://www.theatlantic.com/feed/channel/technology/', category: 'technology' },
+            { title: 'health_news', url: 'https://www.theatlantic.com/feed/channel/health/', category: 'health' },
+            { title: 'sexs_news', url: 'https://www.theatlantic.com/feed/channel/sexes/', category: 'misc' },
+            { title: 'science_news', url: 'https://www.theatlantic.com/feed/channel/science/', category: 'science' },
+            { title: 'press_release_news', url: 'https://www.theatlantic.com/feed/channel/press-releases/', category: 'misc' },
+            { title: 'business_news', url: 'https://www.theatlantic.com/feed/channel/business/', category: 'economy' },
+            { title: 'international_news', url: 'https://www.theatlantic.com/feed/channel/international/', category: 'international' },
+            { title: 'us_news', url: 'https://www.theatlantic.com/feed/channel/national/', category: 'us' },
+            { title: 'top_news', url: 'https://www.theatlantic.com/feed/channel/news/', category: 'top' },
+            { title: 'education_news', url: 'https://www.theatlantic.com/feed/channel/education/', category: 'education' },
         ]
     }
 
     format(obj) {
         var js = JSON.parse(obj.data)
-        // console.log(js)
         var data = js.feed.entry
-        // console.log(data)
         var newData = []
         for (var news of data) {
             var newsObj = {}
             newsObj.title = news['title']['$t']
             newsObj.published_at = new Date(news['published'])
-            newsObj.thumbnail = news['media:content']['url']
+            // thumbnail wasnt always provided
+            if (news['media:content'] && news['media:content']['url']) {
+                newsObj.thumbnail = news['media:content']['url']
+            } else {
+                newsObj.thumbnail = 'http://placehold.it/250x200'
+            }
+            // if no link : https://www.theatlantic.com
+            if (news['link'] && news['link']['href']) {
+                newsObj.url = news['link']['href']
+            } else {
+                newsObj.url = 'https://www.theatlantic.com/'
+            }
 
-            newsObj.url = news['link']['href']
-            // might want to remove all of the HTML stuff from the description?
-            newsObj.description = news['summary']['$t']
+            if (news['summary'] && news['summary']['$t']) {
+                newsObj.description = news['summary']['$t']
+            } else {
+                newsObj.description = 'No description.'
+            }
             newsObj.category = obj.category
             // push the formatted data into newData[]
             newData.push(newsObj)
