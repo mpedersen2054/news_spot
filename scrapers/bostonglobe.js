a8ae6c5cfbd21f77f7d61863b4484860a9b9727f
Matt Pedersen <mpedersen2054@gmail.com>
2017-09-13 19:59:39 -0400

    add all urls for Boston Globe, add flag in npm test to allow it to wait instead of throwing 'before timeout hook' error

--- scrapers/bostonglobe.js
+++ scrapers/bostonglobe.js
@@ -6,7 +6,24 @@ module.exports = class BostonGlobe extends Scraper {
     constructor(urls) {
         super()
         this.urls = [
-            { title: 'us_news', url: 'https://www.boston.com/tag/national-news/?feed=rss', category: 'us' }
+            { title: 'us_news', url: 'https://www.boston.com/tag/national-news/feed', category: 'us' },
+            { title: 'world_news', url: 'https://www.boston.com/tag/world-news/feed', category: 'international' },
+            { title: 'business_news', url: 'https://www.boston.com/tag/business/feed', category: 'economy' },
+            { title: 'education_news', url: 'https://www.boston.com/tag/education/feed', category: 'education' },
+            { title: 'finance_news', url: 'https://www.boston.com/tag/finance/feed', category: 'economy' },
+            { title: 'health_news', url: 'https://www.boston.com/tag/health/feed', category: 'health' },
+            { title: 'history_news', url: 'https://www.boston.com/tag/history/feed', category: 'misc' },
+            { title: 'innovation_news', url: 'https://www.boston.com/tag/innovation/feed', category: 'science' },
+            { title: 'jobs_news', url: 'https://www.boston.com/tag/jobs-news/feed', category: 'economy' },
+            { title: 'politics_news', url: 'https://www.boston.com/tag/politics/feed', category: 'politics' },
+            { title: 'science_news', url: 'https://www.boston.com/tag/science/feed', category: 'science' },
+            { title: 'technology_news', url: 'https://www.boston.com/tag/technology/feed', category: 'technology' },
+            { title: 'arts_news', url: 'https://www.boston.com/tag/arts/feed', category: 'entertainment' },
+            { title: 'entertainment_news', url: 'https://www.boston.com/tag/entertainment/feed', category: 'entertainment' },
+            { title: 'food_news', url: 'https://www.boston.com/tag/food/feed', category: 'entertainment' },
+            { title: 'media_news', url: 'https://www.boston.com/tag/media/feed', category: 'misc' },
+            { title: 'movies_news', url: 'https://www.boston.com/tag/movies/feed', category: 'entertainment' },
+            { title: 'travel_news', url: 'https://www.boston.com/tag/travel/feed', category: 'travel' },
         ]
     }
 
