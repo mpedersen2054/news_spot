33e645b91d289c3a610eeded14acf13efccf25f8
Matt Pedersen <mpedersen2054@gmail.com>
2017-09-18 14:31:34 -0400

    add links for The Economist

--- scrapers/theeconomist.js
+++ scrapers/theeconomist.js
@@ -7,7 +7,19 @@ module.exports = class TheEconomist extends Scraper {
         super()
         this.urls = [
             { title: 'business_news', url: 'http://www.economist.com/sections/business-finance/rss.xml', category: 'economy' },
-            // { title: '', url: '', category: '' },
+            { title: 'economy_news', url: 'http://www.economist.com/sections/economics/rss.xml', category: 'economy' },
+            { title: 'science_news', url: 'http://www.economist.com/sections/science-technology/rss.xml', category: 'science' },
+            { title: 'culture_news', url: 'http://www.economist.com/sections/culture/rss.xml', category: 'misc' },
+            { title: 'us_news', url: 'http://www.economist.com/sections/united-states/rss.xml', category: 'us' },
+            { title: 'uk_news', url: 'http://www.economist.com/sections/britain/rss.xml', category: 'uk' },
+            { title: 'china_news', url: 'http://www.economist.com/sections/china/rss.xml', category: 'cn' },
+            { title: 'america_news', url: 'http://www.economist.com/sections/americas/rss.xml', category: 'us' },
+            { title: 'middleeast_news', url: 'http://www.economist.com/sections/middle-east-africa/rss.xml', category: 'me' },
+            { title: 'obituary_news', url: 'http://www.economist.com/sections/obituary/rss.xml', category: 'misc' },
+            { title: 'leaders_news', url: 'http://www.economist.com/sections/leaders/rss.xml', category: 'misc' },
+            { title: 'international_news', url: 'http://www.economist.com/sections/international/rss.xml', category: 'international' },
+            { title: 'eu_news', url: 'http://www.economist.com/sections/europe/rss.xml', category: 'eu' },
+            { title: 'asia_news', url: 'http://www.economist.com/sections/asia/rss.xml', category: 'cn' },
         ]
     }
 
