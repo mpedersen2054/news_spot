77e45ba6f3182d68e30accbf1b38ff9a5c240464
Matt Pedersen <mpedersen2054@gmail.com>
2017-09-18 14:11:27 -0400

    remove .format from base, and move it into abcnews

--- scrapers/abcnews.js
+++ scrapers/abcnews.js
@@ -24,4 +24,22 @@ module.exports = class ABCNews extends Scraper {
             { title: 'gma_headlines', url: 'http://abcnews.go.com/abcnews/gmaheadlines', category: 'entertainment' },
         ]
     }
+
+    format(obj) {
+        var js = JSON.parse(obj.data)
+        var data = js.rss.channel.item
+        var newData = []
+        for (var news of data) {
+            var newsObj = {}
+            newsObj.title = news['title']
+            newsObj.published_at = new Date(news['pubDate'])
+            newsObj.thumbnail = news['media:thumbnail'][3]
+            newsObj.url = news['link']
+            newsObj.description = news['description']
+            newsObj.category = obj.category
+            // push the formatted data into newData[]
+            newData.push(newsObj)
+        }
+        return JSON.stringify({ title: obj.title, category: obj.category, stories: newData })
+    }
 }
--- scrapers/base.js
+++ scrapers/base.js
@@ -33,22 +33,4 @@ module.exports = class Scraper {
                 })
         })
     }
-
-    format(obj) {
-        var js = JSON.parse(obj.data)
-        var data = js.rss.channel.item
-        var newData = []
-        for (var news of data) {
-            var newsObj = {}
-            newsObj.title = news['title']
-            newsObj.published_at = new Date(news['pubDate'])
-            newsObj.thumbnail = news['media:thumbnail'][3]
-            newsObj.url = news['link']
-            newsObj.description = news['description']
-            newsObj.category = obj.category
-            // push the formatted data into newData[]
-            newData.push(newsObj)
-        }
-        return JSON.stringify({ title: obj.title, category: obj.category, stories: newData })
-    }
 }
