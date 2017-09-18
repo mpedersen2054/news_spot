80e6bf97cbc6b26a92a85c43c53ebb76d31966c9
Matt Pedersen <mpedersen2054@gmail.com>
2017-09-18 18:32:53 -0400

    add all links for The Hill

--- scrapers/thehill.js
+++ scrapers/thehill.js
@@ -7,6 +7,19 @@ module.exports = class TheHill extends Scraper {
         super()
         this.urls = [
             { title: 'top_news', url: 'http://thehill.com/rss/syndicator/19110', category: 'top' },
+            { title: 'administration_news', url: 'http://thehill.com/taxonomy/term/1132/feed', category: 'politics' },
+            { title: 'senate_news', url: 'http://thehill.com/taxonomy/term/1130/feed', category: 'politics' },
+            { title: 'houseofreps_news', url: 'http://thehill.com/taxonomy/term/1131/feed', category: 'politics' },
+            { title: 'polls_news', url: 'http://thehill.com/taxonomy/term/1630/feed', category: 'politics' },
+            { title: 'defense_news', url: 'http://thehill.com/taxonomy/term/39/feed', category: 'us' },
+            { title: 'environment_news', url: 'http://thehill.com/taxonomy/term/28/feed', category: 'science' },
+            { title: 'finance_news', url: 'http://thehill.com/taxonomy/term/30/feed', category: 'economy' },
+            { title: 'healthcare_news', url: 'http://thehill.com/taxonomy/term/33/feed', category: 'politics' },
+            { title: 'technology_news', url: 'http://thehill.com/taxonomy/term/27/feed', category: 'technology' },
+            { title: 'transportation_news', url: 'http://thehill.com/taxonomy/term/38/feed', category: 'us' },
+            { title: 'international_news', url: 'http://thehill.com/taxonomy/term/43/feed', category: 'international' },
+            { title: 'business_news', url: 'http://thehill.com/taxonomy/term/20/feed', category: 'economy' },
+            { title: 'editorials_news', url: 'http://thehill.com/taxonomy/term/1114/feed', category: 'misc' },
         ]
     }
 
@@ -19,7 +32,6 @@ module.exports = class TheHill extends Scraper {
         }
         for (var news of data) {
             var newsObj = {}
-            console.log(news)
             newsObj.title = news['title']
             newsObj.published_at = new Date(news['pubDate'])
             newsObj.thumbnail = 'http://placehold.it/250x200'
