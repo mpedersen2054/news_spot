ee7917f25fff122af4d9711303219a682dd7c1f1
Matt Pedersen <mpedersen2054@gmail.com>
2017-09-27 22:15:36 -0400

    reformat moree tests

--- test/scrapers/theatlanticnews.js
+++ test/scrapers/theatlanticnews.js
@@ -1,68 +1,48 @@
-let assert = require('assert')
-let Atlanticnews = require('../../scrapers/atlanticnews')
-let expect = require('chai').expect
-var obj, dataObj
+let assert = require('assert'),
+    AtlanticNews = require('../../scrapers/atlanticnews'),
+    expect = require('chai').expect,
+    dataObj
 
-describe('Atlanticnews scraper', function() {
-
+describe('AtlanticNews scraper', function() {
     before(function(done) {
-        new Atlanticnews().init(function(retObj) {
-            obj = retObj
+        new AtlanticNews().init().then(retObj => {
+            dataObj = retObj
             done()
         })
     })
 
-    it('should be an instance of Atlanticnews', function() {
-        expect(obj).to.be.an.instanceof(Atlanticnews);
-    })
-
-    it('should have a urls property that is not empty', function() {
-        expect(obj.urls).to.be.an('array').that.is.not.empty
-    })
-
-    it('should have a data property that is not empty', function() {
-        expect(obj.data).to.be.an('array').that.is.not.empty
-    })
-
-    it('data property should have equal number of objects to urls', function() {
-        expect(obj.urls.length).to.equal(obj.data.length)
-    })
-
-
-    describe('Atlanticnews stories object', function() {
-        before(function() {
-            var parsed = obj.data.map(function(data) {
-                return JSON.parse(data)
-            })
-            dataObj = parsed
-        })
+    describe('AtlanticNews stories object', function() {
 
         it('should have atleast one story', function() {
-            expect(dataObj[0].stories).to.have.lengthOf(dataObj[0].stories.length)
+            expect(dataObj).to.have.lengthOf(dataObj.length)
         })
 
         it('story data object should contain a title', function() {
-            expect(dataObj[0].stories[0]).to.have.property('title').that.is.not.empty
+            expect(dataObj[0]).to.have.property('title').that.is.not.empty
         })
 
         it('story data object should contain a published_at', function() {
-            expect(dataObj[0].stories[0]).to.have.property('published_at').that.is.not.empty
+            expect(dataObj[0]).to.have.property('published_at').to.exist
         })
 
         it('story data object should contain a thumbnail', function() {
-            expect(dataObj[0].stories[0]).to.have.property('thumbnail').that.is.not.empty
+            expect(dataObj[0]).to.have.property('thumbnail').that.is.not.empty
         })
 
         it('story data object should contain a url', function() {
-            expect(dataObj[0].stories[0]).to.have.property('url').that.is.not.empty
+            expect(dataObj[0]).to.have.property('url').that.is.not.empty
         })
 
         it('story data object should contain a description', function() {
-            expect(dataObj[0].stories[0]).to.have.property('description').that.is.not.empty
+            expect(dataObj[0]).to.have.property('description').that.is.not.empty
         })
 
         it('story data object should contain a category', function() {
-            expect(dataObj[0].stories[0]).to.have.property('category').that.is.not.empty
+            expect(dataObj[0]).to.have.property('category').that.is.not.empty
+        })
+
+        it('story data object should contain a headline', function() {
+            expect(dataObj[0]).to.have.property('headline').that.is.not.empty
         })
     })
 
