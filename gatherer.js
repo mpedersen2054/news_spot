351b4ee5ae8ba529bc8a06c7ea7edd496a9ed26a
Matt Pedersen <mpedersen2054@gmail.com>
2017-10-05 02:20:40 -0400

    p

--- gatherer.js
+++ gatherer.js
@@ -6,8 +6,8 @@ let scrapers = require('./server/lib/scrapers_load_obj'),
 let outlets = Object.values(scrapers).slice(0)
 
 // will run addOutletStories SYNCRONOUSLY. need this
-// because if running it asyncronously, there will be way
-// too many threads/processes in use and it will throw errors.
+// because if running it asyncronously there will be way
+// too many processes in use and it will throw error.
 mapSeries(outlets, addOutletStories)
     .then((failures) => {
         failures = failures.filter(v => v !== 0)
