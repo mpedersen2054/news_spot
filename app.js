d053f4e0bc9b60249b47c857ed540be276a776c0
Matt Pedersen <mpedersen2054@gmail.com>
2017-10-10 17:17:04 -0400

    test env vars

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ let express = require('express'),
     app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello there news_spot app!')
+    res.send({
+        username: process.env.PROD_DB_USERNAME,
+        password: process.env.PROD_DB_PASSWORD,
+        dbname: process.env.PROD_DB_NAME,
+        hostname: process.env.PROD_DB_HOSTNAME,
+        port:process.env.PROD_DB_PORT,
+    })
 })
 
 app.listen(port, () => console.log(`App running on port ${port}`))
