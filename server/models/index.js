74b49a471c59d34572dbdecb81a075fcdffc1330
Matt Pedersen <mpedersen2054@gmail.com>
2017-10-12 22:44:40 -0400

    add code so if in .env NODE_ENV is production it will query the production db

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
@@ -11,7 +12,7 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable])
 } else {
     sequelize = new Sequelize(
-        config.database, config.username, config.password, config
+        config
     )
 }
 
