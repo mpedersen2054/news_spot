4b6300f3a056194a466f8531d7dd939eb4f1f67d
Matt Pedersen <mpedersen2054@gmail.com>
2017-10-19 21:23:22 -0400

    initial setup for client side

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    path = require('path')
 
 let app = express()
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'client', 'public')))
+// app.use(express.static(path.join(__dirname, 'node_modules')))
 
 // load in the routes
 require('./server/config/routes')(app)
